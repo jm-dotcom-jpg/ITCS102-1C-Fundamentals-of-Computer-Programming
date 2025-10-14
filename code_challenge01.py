f0465629a9cf0d4fceec1fd8074ae8604c765ddb
John Manuel C. Andog <andogjohnmanuel@gmail.com>
2025-10-14 20:47:57 +0800

    Update and rename code_challenge1.py to code_challenge01.py

---
+++ code_challenge01.py
@@ -0,0 +1,6 @@
+name = input("What is your name?:")
+print("Hello",name,"welcome to the matrix")
+
+
+
+print("\n\n\t\t\t\t\t*" "\n\t\t\t\t*" "\t\t*" "\n\t\t\t*" "\t\t\t\t*" "\n\t\t*" "\t\t\t\t\t\t*" "\n\t*" "\t\t\t" "\t""Hi",name,"\t\t\t*" "\n\n\t\t*" "\t\t\t\t\t\t*" "\n\n\t\t\t*" "\t\t\t\t*" "\n\n\t\t\t\t*" "\t\t*" "\n\n\t\t\t\t\t*")
--- code_challenge1.py
+++
@@ -1,5 +0,0 @@
-name = input("What is your name?:")
-print("Hello",name,"welcome to the matrix")
-
-
-print("\n\n\t\t\t\t\t*" "\n\t\t\t\t*" "\t\t*" "\n\t\t\t*" "\t\t\t\t*" "\n\t\t*" "\t\t\t\t\t\t*" "\n\t*" "\t\t\t" "\t""Hi",name,"\t\t\t*" "\n\n\t\t*" "\t\t\t\t\t\t*" "\n\n\t\t\t*" "\t\t\t\t*" "\n\n\t\t\t\t*" "\t\t*" "\n\n\t\t\t\t\t*")
