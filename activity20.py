a626003e700188bd72424b28500387779a29bccf
John Manuel C. Andog <andogjohnmanuel@gmail.com>
2025-10-15 14:23:22 +0800

    Add files via upload

---
+++ activity20.py
@@ -0,0 +1,13 @@
+relapse = True
+
+while relapse == True:
+    relapse_yes = input("Are you nuts?(yes or no?):").lower()
+
+    if relapse_yes == "yes":
+        print("Pagkat ikaw lamang nais makatabiiii, malamig man o mainit ang gabii...")
+        continue
+
+    else:
+        print("Aray koohhh")
+        break
+        relapse_yes = False
